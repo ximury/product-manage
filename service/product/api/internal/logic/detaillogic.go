5f9f82bcd16dc89b85c752579fd5406fecdaf44f
汐木 <18920955769@163.com>
2022-10-08 14:30:01 +0800

    [add]编写product api服务：添加产品详情逻辑

--- service/product/api/internal/logic/detaillogic.go
+++ service/product/api/internal/logic/detaillogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"product/service/product/rpc/pb/product"
 
 	"product/service/product/api/internal/svc"
 	"product/service/product/api/internal/types"
@@ -23,8 +24,20 @@ func NewDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DetailLogi
 	}
 }
 
-func (l *DetailLogic) Detail(req *types.DetailRequest) (resp *types.DetailResponse, err error) {
-	// todo: add your logic here and delete this line
+func (l *DetailLogic) Detail(req types.DetailRequest) (resp *types.DetailResponse, err error) {
+	res, err := l.svcCtx.ProductRpc.Detail(l.ctx, &product.DetailRequest{
+		Id: req.Id,
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	return
+	return &types.DetailResponse{
+		Id:     res.Id,
+		Name:   res.Name,
+		Desc:   res.Desc,
+		Stock:  res.Stock,
+		Amount: res.Amount,
+		Status: res.Status,
+	}, nil
 }
